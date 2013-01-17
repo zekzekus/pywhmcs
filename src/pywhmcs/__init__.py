5501bae88986022ed589417cb566bf13b029c34d
Zekeriya Koc <zekeriyak@ttn.com.tr>
2013-01-17 11:45:56 +0200

    logging facility configured

--- src/pywhmcs/__init__.py
+++ src/pywhmcs/__init__.py
@@ -1,15 +1,11 @@
 import argparse
 import logging as log
 
-log.basicConfig(level=log.DEBUG)
 
-parser = argparse.ArgumentParser(
-    description="pyWHMCS Client Comman Line Interface")
+parser = argparse.ArgumentParser(description="pyWHMCS Client Comman Line Interface")
 parser.add_argument("--debug", action="store_true", help="Activate debugging output")
-parser.add_argument("--url", help="WHMCS API endpoint URL",
-                    required=True)
-parser.add_argument("--username", help="Username with API permission",
-                    required=True)
+parser.add_argument("--url", help="WHMCS API endpoint URL", required=True)
+parser.add_argument("--username", help="Username with API permission", required=True)
 parser.add_argument("--password", help="API user password", required=True)
 parser.add_argument("--action", help="Action name to invoke", required=True)
 parser.add_argument("--params", nargs=argparse.REMAINDER,
@@ -18,18 +14,22 @@ args = parser.parse_args()
 
 
 def main():
-    log.debug("Parameter List:")
+    if args.debug:
+        log.basicConfig(level=log.DEBUG)
+    log.basicConfig(level=log.WARNING)
+
+    log.info("Parameter List:")
     for k, v in args.__dict__.items():
-        log.debug("%s = %s" % (k.upper(), v))
+        log.info("%s = %s" % (k.upper(), v))
 
     params_dict = {}
     for p in args.params:
         p_list = p.split('=')
         if len(p_list) != 2 or p_list[0] == "" or p_list[1] == "":
-            log.debug("Bad formatted --params: %s" % args.params)
+            log.info("Bad formatted --params: %s" % args.params)
             parser.error("Params bad format. Each param must be in param1=value1 format. Your data: %s" % p)
         params_dict[p_list[0]] = p_list[1]
-    log.debug("--params dictionary: %s" % params_dict)
+    log.info("PARAMS dictionary: %s" % params_dict)
 
     invoke(args.url, args.username, args.password, args.action, params_dict)
 
